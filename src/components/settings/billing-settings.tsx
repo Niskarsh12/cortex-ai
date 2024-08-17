40260624537aa5b653dd10d7cdb7d801f38f7329
Niskarsh Dwivedi <niskarshdwivedi12@gmail.com>
2024-08-17 23:38:18 +0530

    push some little changes in settings page

--- src/components/settings/billing-settings.tsx
+++ src/components/settings/billing-settings.tsx
@@ -3,13 +3,23 @@ import React from 'react'
 import Section from '../section-label'
 import { Card, CardContent, CardDescription } from '../ui/card'
 import { Check, CheckCircle2, Plus } from 'lucide-react'
+import { pricingCards } from '@/constants/landing-page'
+//import Modal from '../mondal'
+//import SubscriptionForm from '../forms/settings/subscription-form'
+import Image from 'next/image'
 
 type Props = {}
 
 const BillingSettings = async (props: Props) => {
-    const plan = await onGetSubscriptionPlan()
-    return (
-      <div className="grid grid-cols-1 lg:grid-cols-5 gap-10">
+  const plan = await onGetSubscriptionPlan()
+  const planFeatures = pricingCards.find(
+    (card) => card.title.toUpperCase() === plan?.toUpperCase()
+  )?.features
+  if (!planFeatures) return
+
+  console.log(planFeatures)
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-5 gap-10">
       <div className="lg:col-span-1">
         <Section
           label="Billing settings"
@@ -17,7 +27,8 @@ const BillingSettings = async (props: Props) => {
         />
       </div>
       <div className="lg:col-span-2 flex justify-start lg:justify-center ">
-      <Card className="border-dashed bg-cream border-gray-400 w-full cursor-pointer h-[270px] flex justify-center items-center">
+      
+              <Card className="border-dashed bg-cream border-gray-400 w-full cursor-pointer h-[270px] flex justify-center items-center">
                 <CardContent className="flex gap-2 items-center">
                   <div className="rounded-full border-2 p-1">
                     <Plus className="text-gray-400" />
@@ -27,10 +38,26 @@ const BillingSettings = async (props: Props) => {
                   </CardDescription>
                 </CardContent>
               </Card>
-            </div>
+            
+         {/* <SubscriptionForm plan={plan!} /> */}
       </div>
-    )
-
-} 
+      <div className="lg:col-span-2">
+        <h3 className="text-xl font-semibold mb-2">Current Plan</h3>
+        <p className="text-sm font-semibold">{plan}</p>
+        <div className="flex gap-2 flex-col mt-2">
+          {planFeatures.map((feature) => (
+            <div
+              key={feature}
+              className="flex gap-2"
+            >
+              <CheckCircle2 className="text-muted-foreground" />
+              <p className="text-muted-foreground">{feature}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default BillingSettings
